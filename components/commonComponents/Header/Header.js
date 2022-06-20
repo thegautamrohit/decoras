d76e26339430b382dfcb9a49eee90e2a015cc307
Ajay Upadhyay <ajay.bhumca18@gmail.com>
2022-06-20 17:36:42 +0530

    header added

--- components/commonComponents/Header/Header.js
+++ components/commonComponents/Header/Header.js
@@ -6,14 +6,53 @@ import { useRouter } from "next/router";
 function Header({ children, href }) {
   const router = useRouter();
 
-  console.log(href, router.asPath);
   return (
     <div className="main__header">
       <div className="main__header__link">
-        <Link href="/">Home</Link>
-        <Link href="/">Our Collection</Link>
-        <Link href="/">About Us</Link>
-        <Link href="/">Contact Us</Link>
+        <Link href="/">
+          <a
+            style={
+              router.asPath === "/"
+                ? { color: "var(--black)" }
+                : { color: "var(--beige)" }
+            }
+          >
+            Home
+          </a>
+        </Link>
+        <Link href="/our-collection">
+          <a
+            style={
+              router.asPath === "/"
+                ? { color: "var(--black)" }
+                : { color: "var(--beige)" }
+            }
+          >
+            Our Collection
+          </a>
+        </Link>
+        <Link href="/about-us">
+          <a
+            style={
+              router.asPath === "/"
+                ? { color: "var(--black)" }
+                : { color: "var(--beige)" }
+            }
+          >
+            About Us
+          </a>
+        </Link>
+        <Link href="/contact-us">
+          <a
+            style={
+              router.asPath === "/"
+                ? { color: "var(--black)" }
+                : { color: "var(--beige)" }
+            }
+          >
+            Contact Us
+          </a>
+        </Link>
       </div>
       <div className="main__header__logo">
         <Image layout="intrinsic" src={Logo} />
