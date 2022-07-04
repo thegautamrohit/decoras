2cb83001a65cdd55f913fa985b796b8347ad4fa7
thegautamrohit <rgautam1997@gmail.com>
2022-07-04 17:59:40 +0530

    data update

--- components/ProductData.js
+++ components/ProductData.js
@@ -1,4 +1,69 @@
+// Bags and Stationary Import
+
+import Bags1 from "../Assets/Cut outs/Decoras images cut out/Bags and Stationary/Bags Cutout/BG5609.png";
+import Bags2 from "../Assets/Cut outs/Decoras images cut out/Bags and Stationary/Bags Cutout/BG5610.png";
+import Bags3 from "../Assets/Cut outs/Decoras images cut out/Bags and Stationary/Bags Cutout/BG5611.png";
+import Bags4 from "../Assets/Cut outs/Decoras images cut out/Bags and Stationary/Bags Cutout/BG5612.png";
+import Bags5 from "../Assets/Cut outs/Decoras images cut out/Bags and Stationary/Bags Cutout/BG5613.png";
+
+import Gift1 from "../Assets/Cut outs/Decoras images cut out/Bags and Stationary/Gift warp cutout/GW2013.jpg";
+import Gift2 from "../Assets/Cut outs/Decoras images cut out/Bags and Stationary/Gift warp cutout/GW2014.png";
+import Gift3 from "../Assets/Cut outs/Decoras images cut out/Bags and Stationary/Gift warp cutout/GW2015.png";
+import Gift4 from "../Assets/Cut outs/Decoras images cut out/Bags and Stationary/Gift warp cutout/GW2016.png";
+import Gift5 from "../Assets/Cut outs/Decoras images cut out/Bags and Stationary/Gift warp cutout/GW2310.JPG";
+import Gift6 from "../Assets/Cut outs/Decoras images cut out/Bags and Stationary/Gift warp cutout/GW2312.jpg";
+import Gift7 from "../Assets/Cut outs/Decoras images cut out/Bags and Stationary/Gift warp cutout/GW2313.jpg";
+import Gift8 from "../Assets/Cut outs/Decoras images cut out/Bags and Stationary/Gift warp cutout/GW2314.jpg";
+
+import Stationary1 from "../Assets/Cut outs/Decoras images cut out/Bags and Stationary/stationary cutout/STS01.png";
+import Stationary2 from "../Assets/Cut outs/Decoras images cut out/Bags and Stationary/stationary cutout/STS02.png";
+import Stationary3 from "../Assets/Cut outs/Decoras images cut out/Bags and Stationary/stationary cutout/STS03.png";
+import Stationary4 from "../Assets/Cut outs/Decoras images cut out/Bags and Stationary/stationary cutout/STS04.png";
+import Stationary5 from "../Assets/Cut outs/Decoras images cut out/Bags and Stationary/stationary cutout/STS05.png";
+import Stationary6 from "../Assets/Cut outs/Decoras images cut out/Bags and Stationary/stationary cutout/STS06.png";
+
+// Decor Import
+
+import Bathroom1 from "../Assets/Cut outs/Decoras images cut out/Decor/Bathroom accessories/BR4001.png";
+import Bathroom2 from "../Assets/Cut outs/Decoras images cut out/Decor/Bathroom accessories/BR4002.png";
+import Bathroom3 from "../Assets/Cut outs/Decoras images cut out/Decor/Bathroom accessories/BR4003.png";
+import Bathroom4 from "../Assets/Cut outs/Decoras images cut out/Decor/Bathroom accessories/BR4004.png";
+import Bathroom5 from "../Assets/Cut outs/Decoras images cut out/Decor/Bathroom accessories/BR4005.png";
+import Bathroom6 from "../Assets/Cut outs/Decoras images cut out/Decor/Bathroom accessories/BR4006.png";
+import Bathroom7 from "../Assets/Cut outs/Decoras images cut out/Decor/Bathroom accessories/BR4007.png";
+import Bathroom8 from "../Assets/Cut outs/Decoras images cut out/Decor/Bathroom accessories/BR4008.png";
+import Bathroom9 from "../Assets/Cut outs/Decoras images cut out/Decor/Bathroom accessories/BR4009.png";
+import Bathroom10 from "../Assets/Cut outs/Decoras images cut out/Decor/Bathroom accessories/BR4010.png";
+
+import DoorKnob1 from "../Assets/Cut outs/Decoras images cut out/Decor/Door Knobs/DK1001.png";
+import DoorKnob2 from "../Assets/Cut outs/Decoras images cut out/Decor/Door Knobs/DK1002.png";
+import DoorKnob3 from "../Assets/Cut outs/Decoras images cut out/Decor/Door Knobs/DK1003.png";
+import DoorKnob4 from "../Assets/Cut outs/Decoras images cut out/Decor/Door Knobs/DK1004.png";
+import DoorKnob5 from "../Assets/Cut outs/Decoras images cut out/Decor/Door Knobs/DK1005.png";
+import DoorKnob6 from "../Assets/Cut outs/Decoras images cut out/Decor/Door Knobs/DK1006.png";
+
+import PhotoFrame1 from "../Assets/Cut outs/Decoras images cut out/Decor/Photo Frames/PF3001.png";
+import PhotoFrame2 from "../Assets/Cut outs/Decoras images cut out/Decor/Photo Frames/PF3002.png";
+import PhotoFrame3 from "../Assets/Cut outs/Decoras images cut out/Decor/Photo Frames/PF3003.png";
+import PhotoFrame4 from "../Assets/Cut outs/Decoras images cut out/Decor/Photo Frames/PF3004.png";
+
+import Candles1 from "../Assets/Cut outs/Decoras images cut out/Decor/Scented candles/SC4001.png";
+import Candles2 from "../Assets/Cut outs/Decoras images cut out/Decor/Scented candles/SC4002.png";
+import Candles3 from "../Assets/Cut outs/Decoras images cut out/Decor/Scented candles/SC4003.png";
+import Candles4 from "../Assets/Cut outs/Decoras images cut out/Decor/Scented candles/SC4004.png";
+import Candles5 from "../Assets/Cut outs/Decoras images cut out/Decor/Scented candles/SC4005.png";
+import Candles6 from "../Assets/Cut outs/Decoras images cut out/Decor/Scented candles/SC4006.png";
+import Candles7 from "../Assets/Cut outs/Decoras images cut out/Decor/Scented candles/SC4007.png";
+import Candles8 from "../Assets/Cut outs/Decoras images cut out/Decor/Scented candles/SC4008.png";
+import Candles9 from "../Assets/Cut outs/Decoras images cut out/Decor/Scented candles/SC4009.png";
+import Candles10 from "../Assets/Cut outs/Decoras images cut out/Decor/Scented candles/SC40010.png";
+import Candles11 from "../Assets/Cut outs/Decoras images cut out/Decor/Scented candles/SC40011.png";
+import Candles12 from "../Assets/Cut outs/Decoras images cut out/Decor/Scented candles/SC40012.png";
+import Candles13 from "../Assets/Cut outs/Decoras images cut out/Decor/Scented candles/SC40013.png";
+
 const productData = [
+  // Bags and Stationary
+
   {
     id: 1,
     category: "Bags and Stationary",
@@ -6,17 +71,104 @@ const productData = [
       {
         id: 1,
         category: "Bags",
-        images: [],
+        images: [
+          { id: 1, code: "BG5609", img: Bags1 },
+          { id: 2, code: "BG5610", img: Bags2 },
+          { id: 3, code: "BG5611", img: Bags3 },
+          { id: 4, code: "BG5612", img: Bags4 },
+          { id: 5, code: "BG5613", img: Bags5 },
+        ],
       },
       {
         id: 2,
         category: "Gift Wrap",
-        images: [],
+        images: [
+          { id: 1, code: "GW2013", img: Gift1 },
+          { id: 2, code: "GW2014", img: Gift2 },
+          { id: 3, code: "GW2015", img: Gift3 },
+          { id: 4, code: "GW2016", img: Gift4 },
+          { id: 5, code: "GW2310", img: Gift5 },
+          { id: 6, code: "GW2312", img: Gift6 },
+          { id: 7, code: "GW2313", img: Gift7 },
+          { id: 8, code: "GW2314", img: Gift8 },
+        ],
       },
       {
         id: 1,
         category: "Stationary",
-        images: [],
+        images: [
+          { id: 1, code: "STS01", img: Stationary1 },
+          { id: 2, code: "STS02", img: Stationary2 },
+          { id: 3, code: "STS03", img: Stationary3 },
+          { id: 4, code: "STS04", img: Stationary4 },
+          { id: 5, code: "STS05", img: Stationary5 },
+          { id: 6, code: "STS06", img: Stationary6 },
+        ],
+      },
+    ],
+  },
+
+  // Decor
+  {
+    id: 2,
+    category: "Decor",
+    sub_category: [
+      {
+        id: 1,
+        category: "Bathroom Accessories",
+        images: [
+          { id: 1, code: "BR4001", img: Bathroom1 },
+          { id: 2, code: "BR4002", img: Bathroom2 },
+          { id: 3, code: "BR4003", img: Bathroom3 },
+          { id: 4, code: "BR4004", img: Bathroom4 },
+          { id: 5, code: "BR4005", img: Bathroom5 },
+          { id: 6, code: "BR4006", img: Bathroom6 },
+          { id: 7, code: "BR4007", img: Bathroom7 },
+          { id: 8, code: "BR4008", img: Bathroom8 },
+          { id: 9, code: "BR4009", img: Bathroom9 },
+          { id: 10, code: "BR40010", img: Bathroom10 },
+        ],
+      },
+      {
+        id: 2,
+        category: "Door Knobs",
+        images: [
+          { id: 1, code: "DK1001", img: DoorKnob1 },
+          { id: 2, code: "DK1002", img: DoorKnob2 },
+          { id: 3, code: "DK1003", img: DoorKnob3 },
+          { id: 4, code: "DK1004", img: DoorKnob4 },
+          { id: 5, code: "DK1005", img: DoorKnob5 },
+          { id: 6, code: "DK1006", img: DoorKnob6 },
+        ],
+      },
+      {
+        id: 3,
+        category: "Photo Frames",
+        images: [
+          { id: 1, code: "PF3001", img: PhotoFrame1 },
+          { id: 2, code: "PF3002", img: PhotoFrame2 },
+          { id: 3, code: "PF3003", img: PhotoFrame3 },
+          { id: 4, code: "PF3004", img: PhotoFrame4 },
+        ],
+      },
+      {
+        id: 3,
+        category: "Scented Candles",
+        images: [
+          { id: 1, code: "SC4001", img: Candles1 },
+          { id: 2, code: "SC4002", img: Candles2 },
+          { id: 3, code: "SC4003", img: Candles3 },
+          { id: 4, code: "SC4004", img: Candles4 },
+          { id: 5, code: "SC4005", img: Candles5 },
+          { id: 6, code: "SC4006", img: Candles6 },
+          { id: 7, code: "SC4007", img: Candles7 },
+          { id: 8, code: "SC4008", img: Candles8 },
+          { id: 9, code: "SC4009", img: Candles9 },
+          { id: 10, code: "SC40010", img: Candles10 },
+          { id: 11, code: "SC40011", img: Candles11 },
+          { id: 12, code: "SC40012", img: Candles12 },
+          { id: 13, code: "SC40013", img: Candles13 },
+        ],
       },
     ],
   },
