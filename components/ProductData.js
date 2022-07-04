df7c8487439e6f0ec534f05a265055a25f27c0dd
thegautamrohit <rgautam1997@gmail.com>
2022-07-04 15:48:27 +0530

    data

---
+++ components/ProductData.js
@@ -0,0 +1,23 @@
+const productData = [
+  {
+    id: 1,
+    category: "Bags and Stationary",
+    sub_category: [
+      {
+        id: 1,
+        category: "Bags",
+        images: [],
+      },
+      {
+        id: 2,
+        category: "Gift Wrap",
+        images: [],
+      },
+      {
+        id: 1,
+        category: "Stationary",
+        images: [],
+      },
+    ],
+  },
+];
