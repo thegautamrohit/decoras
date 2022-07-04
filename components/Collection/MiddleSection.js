ea74e34518a86eff6831ba46602b459555c512f6
Ajay Upadhyay <ajay.bhumca18@gmail.com>
2022-07-05 01:22:23 +0530

    middle section almost done

--- components/Collection/MiddleSection.js
+++ components/Collection/MiddleSection.js
@@ -1,38 +1,66 @@
 import React, { useState } from "react";
 import Card from "../commonComponents/Cards/Card";
-import { ProductData } from "../ProductData";
+import { IoChevronBackCircleOutline } from "react-icons/io5";
 
-const MiddleSection = ({ selected, category }) => {
-  console.log(ProductData);
+const MiddleSection = ({
+  selected,
+  category,
+  categoryArray,
+  shuffledArray,
+}) => {
   return (
     <div className="middle__section__container">
       <div
         className="middle__section__chips__container"
-        // style={{ gridTemplateColumns: `repeat(${Chips.length} , 1fr)` }}
+        style={{
+          gridTemplateColumns: `repeat(${
+            categoryArray.length + 10
+          } , max-content)`,
+        }}
       >
-        {/* {Chips.map((chips, index) => {
+        {category.clicked && (
+          <>
+            <IoChevronBackCircleOutline style={{ fontSize: 44 }} />
+
+            <div
+              className="middle__section__chips"
+              style={
+                category.clicked
+                  ? { backgroundColor: "var(--beige)", fontWeight: "medium" }
+                  : {}
+              }
+            >
+              <p>{category.name}</p>
+            </div>
+          </>
+        )}
+        {categoryArray.map((chips, index) => {
           return (
             <div
               className="middle__section__chips"
+              style={
+                category.clicked
+                  ? { pointerEvents: "none" }
+                  : { cursor: "pointer" }
+              }
               key={index}
               onClick={() =>
-                selected({ name: chips, clicked: true, id: index })
-              }
-              style={
-                category.clicked && category.id === index
-                  ? { backgroundColor: "var(--beige)", fontWeight: "medium" }
-                  : {}
+                selected({
+                  name: chips,
+                  clicked: true,
+                  id: index,
+                })
               }
             >
               <p>{chips}</p>
             </div>
           );
-        })} */}
+        })}
       </div>
       <div className="middle__section__cards__container">
-        {/* {data.map((cards, index) => (
+        {shuffledArray.map((cards, index) => (
           <Card data={cards} key={index} />
-        ))} */}
+        ))}
       </div>
     </div>
   );
