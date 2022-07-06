c39cc520ab8d55ce0be34915cb366f5c510adbf1
Ajay Upadhyay <ajay.bhumca18@gmail.com>
2022-07-06 12:32:06 +0530

    gitignore

---
+++ test.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+
+const Path = require("path");
+
+// const recursivePath = (path) => {
+//   const dir = fs.readdirSync(path);
+//   if()
+//   console.log(dir);
+// };
+
+recursivePath("Assets");
