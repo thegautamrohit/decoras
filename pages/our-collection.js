85bb896af0ff6d538068d72cccb050614a9913a8
Ajay Upadhyay <ajay.bhumca18@gmail.com>
2022-07-06 17:06:35 +0530

    header footer fixed our collection page done

--- pages/our-collection.js
+++ pages/our-collection.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
-import Banner from "../components/Collection/Banner";
+import dynamic from "next/dynamic";
 import MiddleSection from "../components/Collection/MiddleSection";
 import { ProductData } from "../components/ProductData";
 import { filterCategory, shuffleArray, filterArray } from "../helper/helper";
 
+const Banner = dynamic(() => import("../components/Collection/Banner"), {
+  ssr: true,
+});
 const shallowCopy = JSON.parse(JSON.stringify(ProductData));
 
 const shuffledArray = shuffleArray(shallowCopy);
@@ -12,6 +15,11 @@ const categoryArray = filterCategory(ProductData);
 
 const Collection = () => {
   const [category, setCategory] = useState({ name: "", clicked: false, id: 0 });
+  const [sub_category, setSub_Category] = useState({
+    name: "",
+    clicked: false,
+    id: 0,
+  });
   const [shuffled, setShuffled] = useState([]);
   const [categorizedArray, setCategorizedArray] = useState([]);
 
@@ -28,7 +36,7 @@ const Collection = () => {
     ).map((item) => item.sub_category);
     const filteredData = filterArray(filterCategory);
 
-    setCategorizedArray([...filteredData]);
+    setCategorizedArray([`${data.name}`, ...filteredData]);
     const getSpecificCategory = ProductData.filter(
       (item) => item.category === data.name
     );
@@ -36,9 +44,20 @@ const Collection = () => {
     setShuffled([...getSpecificCategory]);
   };
   const selectedCategorySub = (data) => {
-    const getSpecificCategory = ProductData.filter(
-      (item) => item.sub_category === data.name
-    );
+    let getSpecificCategory = [];
+    setSub_Category(data);
+
+    console.log(data.name, categorizedArray[0]);
+    if (data.name === categorizedArray[0]) {
+      getSpecificCategory = ProductData.filter(
+        (item) => item.category === data.name
+      );
+    } else {
+      getSpecificCategory = ProductData.filter(
+        (item) => item.sub_category === data.name
+      );
+    }
+
     shuffleArray(getSpecificCategory);
     setShuffled([...getSpecificCategory]);
   };
@@ -47,7 +66,9 @@ const Collection = () => {
     setCategorizedArray(categoryArray);
     setShuffled(shuffledArray);
     setCategory({ name: "", clicked: false, id: 0 });
+    setSub_Category({ name: "", clicked: false, id: 0 });
   };
+
   return (
     <div className="our__collection">
       <Banner name={category.name} />
@@ -55,6 +76,7 @@ const Collection = () => {
         selected={(data) => selectedCategory(data)}
         selectedSub={(data) => selectedCategorySub(data)}
         category={category}
+        sub_category={sub_category}
         categoryArray={categorizedArray}
         shuffledArray={shuffled}
         back={() => goBack()}
