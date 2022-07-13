6c80814951860f5a61a9c5e94a1a01249b5b383f
thegautamrohit <rgautam1997@gmail.com>
2022-07-13 10:38:54 +0530

    about carousel

--- components/About/AboutMission.js
+++ components/About/AboutMission.js
@@ -1,13 +1,31 @@
 import React from "react";
 import Image from "next/image";
+import Fade from "react-reveal/Fade";
 import About1 from "../../Assets/Banner/desktop_banner/banner5.png";
 
 function AboutMission() {
   return (
     <div className="about__content__mission__container">
-      <div className="about__content__content">
-        <h1>OUR MISSION</h1>
-        <div className="about__mission__mob__image">
+      <Fade bottom big>
+        <div className="about__content__content">
+          <h1>OUR MISSION</h1>
+          <div className="about__mission__mob__image">
+            <Image
+              alt="Decoras"
+              src={About1}
+              height={351}
+              width={600}
+              layout="responsive"
+            />
+          </div>
+          <p>
+            We believe that healthy ideas flourish in an healthy environment,
+            which is why we have created an stress-free way to bring serenity
+            into your home, through our signature collections of home decor
+            products.
+          </p>
+        </div>
+        <div className="about__content__image">
           <Image
             alt="Decoras"
             src={About1}
@@ -16,21 +34,7 @@ function AboutMission() {
             layout="responsive"
           />
         </div>
-        <p>
-          We believe that healthy ideas flourish in an healthy environment,
-          which is why we have created an stress-free way to bring serenity into
-          your home, through our signature collections of home decor products.
-        </p>
-      </div>
-      <div className="about__content__image">
-        <Image
-          alt="Decoras"
-          src={About1}
-          height={351}
-          width={600}
-          layout="responsive"
-        />
-      </div>
+      </Fade>
     </div>
   );
 }
--- pages/about.js
+++ pages/about.js
@@ -12,9 +12,9 @@ function about() {
       <Fade bottom big>
         <AboutContent />
       </Fade>
-      <Fade bottom big>
-        <AboutMission />
-      </Fade>
+      {/* <Fade bottom big> */}
+      <AboutMission />
+      {/* </Fade> */}
     </>
   );
 }
