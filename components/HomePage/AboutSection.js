8d5a3573f07697f54d2b8b35ddb3e9602ac631df
thegautamrohit <rgautam1997@gmail.com>
2022-07-13 17:12:00 +0530

    home page changes

--- components/HomePage/AboutSection.js
+++ components/HomePage/AboutSection.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { ParallaxProvider } from "react-scroll-parallax";
 import Image from "next/image";
 import About from "../../Assets/Homepage/About/about.png";
 import Mission from "../../Assets/Homepage/About/mission.png";
@@ -6,46 +7,48 @@ import Button from "../commonComponents/Button/Button";
 
 function AboutSection() {
   return (
-    <div className="about__section__container">
-      <div className="about__section__mono__slab">
-        <div className="about__mono__slab__content_1">
-          <h1>About Us</h1>
-          <p>
-            We are fueled by passion for design and a desire to help others
-            create a space they love.
-          </p>
-          <Button title="Read More" />
-        </div>
-        <div className="about__mono__slab__image_1">
-          {/* <Image
+    <ParallaxProvider speed={-10}>
+      <div className="about__section__container">
+        <div className="about__section__mono__slab">
+          <div className="about__mono__slab__content_1">
+            <h1>About Us</h1>
+            <p>
+              We are fueled by passion for design and a desire to help others
+              create a space they love.
+            </p>
+            <Button title="Read More" />
+          </div>
+          <div className="about__mono__slab__image_1">
+            {/* <Image
             alt="Decoras"
             src={About}
             className="about__mono__slab__image__img"
             layout="fill"
           /> */}
+          </div>
         </div>
-      </div>
-      <div className="about__section__mono__slab">
-        <div className="about__mono__slab__content_2">
-          <h1>Our Mission</h1>
-          <p>
-            We believe that healthy ideas flourish in an healthy environment,
-            which is why we have created an stress-free way to bring serenity
-            into your home, through our signature collections of home decor
-            products.
-          </p>
-          <Button title="Read More" />
-        </div>
-        <div className="about__mono__slab__image_2">
-          {/* <Image
+        <div className="about__section__mono__slab">
+          <div className="about__mono__slab__content_2">
+            <h1>Our Mission</h1>
+            <p>
+              We believe that healthy ideas flourish in an healthy environment,
+              which is why we have created an stress-free way to bring serenity
+              into your home, through our signature collections of home decor
+              products.
+            </p>
+            {/* <Button title="Read More" /> */}
+          </div>
+          <div className="about__mono__slab__image_2">
+            {/* <Image
             alt="Decoras"
             src={Mission}
             className="about__mono__slab__image__img"
             layout="fill"
           /> */}
+          </div>
         </div>
       </div>
-    </div>
+    </ParallaxProvider>
   );
 }
 
