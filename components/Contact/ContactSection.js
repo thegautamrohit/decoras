9882a9363dfff66940a0b33edbe020545a871cfb
thegautamrohit <rgautam1997@gmail.com>
2022-07-15 13:04:53 +0530

    images updated

--- components/Contact/ContactSection.js
+++ components/Contact/ContactSection.js
@@ -6,7 +6,7 @@ function ContactSection() {
       <h1 className="contact__store__name">Store Name</h1>
 
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d230959.4016437163!2d77.09242456354774!3d28.509442124431633!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cfd5b347eb62d%3A0x52c2b7494e204dce!2sNew%20Delhi%2C%20Delhi!5e0!3m2!1sen!2sin!4v1656588791043!5m2!1sen!2sin"
+        src="https://www.google.com/maps/place/12+Daniel+Rd,+Fairfield,+NJ+07004,+USA/@40.8842547,-74.277157,17z/data=!3m1!4b1!4m5!3m4!1s0x89c3010569fc04f5:0x8f5062f512bb5bae!8m2!3d40.8842507!4d-74.2749683"
         width={600}
         height={450}
         style={{ border: 0 }}
@@ -20,16 +20,16 @@ function ContactSection() {
 
         <div className="contact__store__info">
           <h4>Address</h4>
-          <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr</p>
+          <p>12, Daniel Road, Fairfield, New Jersey 07004</p>
           <h4>Phone Number</h4>
-          <p>9876543210</p>
+          <p>+1-862-505-1710</p>
           <h4>HOURS</h4>
           <p>9:00-7:00</p>
           <p>9:00-7:00</p>
           <p>9:00-7:00</p>
 
           <h4>EMAIL</h4>
-          <p>email@email.com</p>
+          <p>info@decorasusa.com</p>
         </div>
       </div>
     </div>
